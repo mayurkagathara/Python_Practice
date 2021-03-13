84a249bb47155cf0f73e200e796dc72c0fc2b346
mayurkagathara <mayur.kagathara2508@gmail.com>
2021-03-13 16:23:45 +0530

    plot sortin algorithm

--- Blogger/plotting.py
+++ Blogger/plotting.py
@@ -10,5 +10,5 @@ def plot_array(arr,title='title',no_of_comparison=0,no_of_swaps=0):
   ax.text(0,40,data)
   fig.canvas.draw()
   fig.canvas.flush_events()
-  plt.pause(0.001)
+  plt.pause(0.00001)
   ax.clear()
