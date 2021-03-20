191242efcd47f3d4bd3f705187584c35eedccb6e
mayurkagathara <mayur.kagathara2508@gmail.com>
2021-03-20 16:41:49 +0530

    Python practice

--- Blogger/bin.py
+++ Blogger/bin.py
@@ -44,7 +44,65 @@
 # for c in combs:
 #   print(c)
 
+
+
 # %%
+def digits(number):
+    # pythonic way
+    number_str = str(number)
+    digits_arr = list(map(int,number_str))
+    return digits_arr
+
+    #Standard way coming soon
 
 
+# %%    
+def multiply(arr,x):
+    """
+    To multiply the array arr as number with x and retutn array as answer.
+    ex. [1,2,3]*5 = [6,1,5]
+    """
+    print(arr,x)
+    multiplication_arr = []
+    current_carry = 0
+    for number in arr[::-1]: #array in reverse order
+        multiplication = number*x + current_carry
+        current_carry = (multiplication - multiplication%10)//10
+        unit_digit = multiplication - (current_carry*10)
+        multiplication_arr.insert(0,unit_digit)
+        print(number,multiplication,unit_digit,current_carry)
+        print(multiplication_arr)
+
+    #to check if we have carry while multiplying last digit
+    if current_carry:
+        digits_in_current_carry = digits(current_carry)
+        for digit in digits_in_current_carry[::-1]:
+            multiplication_arr.insert(0,digit)
+    return multiplication_arr
+multiply([8, 7, 1, 7, 8, 2, 9, 1, 2, 0, 0], 15)
+
+# %%
+def big_factorial(n):
+    """
+    To find the factorial of a big number considering there is no bignum in the python using array.
+    We take assumption that our factorial consumes more bits than available in the largest datatype available.
+    """
+    if n==0:
+        return 1
+
+    answer = [1]
+    for x in range(2,n+1):
+        answer = multiply(answer,x)
+    
+    return(answer)
+
+big_factorial(15)
+
+# %%
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+    
+factorial(20)
 # %%
