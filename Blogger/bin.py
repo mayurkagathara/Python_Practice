28b713c7064d97c5816121511e370825a5de2534
mayurkagathara <mayur.kagathara2508@gmail.com>
2021-03-20 15:39:31 +0530

    python foundation

---
+++ Blogger/bin.py
@@ -0,0 +1,50 @@
+# i= [12,9,14]
+# k=[7,16,11]
+
+# for j in i[:]: 
+#   for m in k[:]:
+#     if (j%m==0):
+#       j=j//2
+#       m=m/2
+#       print(j,m)
+#     else:
+#       j=j+m
+#       m=m-j
+#       print(j,m)
+
+# class A:
+#   def __init__(self,i=0):
+#     self.i = i
+
+# class B(A):
+#   def __init__(self,j=2):
+#     super().__init__()
+#     self.j=j
+
+# def main():
+#   b= B(50)
+#   print(b.i)
+#   print(b.j)
+# main()
+
+# try:
+#   fh = open("sasas","w")
+#   fh.write("asdasd")
+# else:
+#   print("asdasd")
+# except:
+#   print(12412)
+# finally:
+#   print("asdasdasd")
+#   fh.close()
+
+# verification
+# from itertools import combinations
+# combs = combinations(arr,3)
+# for c in combs:
+#   print(c)
+
+# %%
+
+
+# %%
