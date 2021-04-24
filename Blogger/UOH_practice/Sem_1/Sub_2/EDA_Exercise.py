eb13e695da832f05c4147d98a7db0fe444c8c381
mayurkagathara <mayur.kagathara2508@gmail.com>
2021-04-24 11:44:11 +0530

    draft copy

--- Blogger/UOH_practice/Sem_1/Sub_2/EDA_Exercise.py
+++ Blogger/UOH_practice/Sem_1/Sub_2/EDA_Exercise.py
@@ -4,8 +4,9 @@ from scipy.stats import median_abs_deviation,iqr
 import math
 np.random.seed(100)
 
-array = np.random.randint(0,1000,size=1000)
+# array = np.random.randint(0,1000,size=1000)
 # array = [12,2,33,1,41,1,26,3,64,45,5,65,6,26,73]
+array = [1,2,3,4,5,6,7,8,9,10]
 print(f"{'Mean':>25}",np.mean(array))
 print(f"{'Median':>25}",np.median(array))
 print(f"{'Variance':>25}",np.var(array))
@@ -30,6 +31,7 @@ Median Absolute Deviation 383.2521
 def mean(arr):
     return sum(arr)/len(arr)
 def median(arr):
+    # check Smart way
     sorted_arr = sorted(arr)
     if len(arr)%2==1:
         return sorted_arr[math.ceil(len(sorted_arr)/2) - 1]
@@ -37,6 +39,10 @@ def median(arr):
         left = (len(sorted_arr)//2) - 1
         right = (len(sorted_arr)//2)
         return mean([sorted_arr[left],sorted_arr[right]])
+    
+    # Smart way
+    # return percentile(arr, 50)
+
 def variance(arr):
     mean_value = mean(arr)
     variance_value = 0
@@ -45,12 +51,20 @@ def variance(arr):
     return variance_value/len(arr)
 def stdev(arr):
     return math.sqrt(variance(arr))
-def quantile(arr, kth):
+def percentile(arr, kth):
+    if kth==0:
+        return 0
+    
     sorted_arr = sorted(arr)
-    kth_index = math.ceil(len(arr)*(kth/100)) - 1
-    return sorted_arr[kth_index]
+    # linear interpolation
+    left_ind = math.floor((len(arr)+1)*(kth/100)) - 1
+    right_ind = math.ceil((len(arr)+1)*(kth/100)) - 1
+    kth_precentile = mean([sorted_arr[left_ind],sorted_arr[right_ind]])
+    return kth_precentile
+
+
 def IQR(arr):
-    return quantile(arr,75) - quantile(arr,25)
+    return percentile(arr,75) - percentile(arr,25)
 def MAD(arr):
     median_value = median(arr)
     MADiv = [abs(ele - median_value) for ele in arr]
@@ -63,7 +77,7 @@ print(f"{'Median':>15}",median(array))
 print(f"{'Variance':>15}",variance(array))
 print(f"{'Standard Deviation':>15}", stdev(array))
 print(f"{'IQR':>15}",IQR(array))
-print(f"{'90perc':>15}",quantile(array,90))
-print(f"{'99perc':>15}",quantile(array,99))
+print(f"{'90perc':>15}",percentile(array,90))
+print(f"{'99perc':>15}",percentile(array,99))
 print(f"{'MAD':>15}",MAD(array))
 # %%
